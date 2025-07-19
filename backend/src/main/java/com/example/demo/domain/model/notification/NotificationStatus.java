8caa4eb1ce339d8b294b66f4f1b98ac87292c484
robinjoon <robin980108@naver.com>
2025-07-19 14:31:48 +0900

    feat: NotificationEntity, Repository, Mapper 생성

--- backend/src/main/java/com/example/demo/domain/model/notification/NotificationStatus.java
+++ backend/src/main/java/com/example/demo/domain/model/notification/NotificationStatus.java
@@ -13,6 +13,10 @@ public final class NotificationStatus {
     private final ReadStatus status;
     private final LocalDateTime readAt;
 
+    public static NotificationStatus of(ReadStatus status, LocalDateTime readAt) {
+        return new NotificationStatus(status, readAt);
+    }
+
     public static NotificationStatus unread() {
         return new NotificationStatus(ReadStatus.UNREAD, null);
     }
