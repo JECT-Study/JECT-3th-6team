33e307bb9b8930439883002ed08044282ebcea31
jjeongsu <jeongsu.dev@gmail.com>
2025-07-21 13:08:22 +0900

    chore : 사용하지 않은 함수 주석처리

--- frontend/src/entities/user/hook/useInitUser.ts
+++ frontend/src/entities/user/hook/useInitUser.ts
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 // cookie의 토큰과 전역 유저상태의 sync를 맞추기 위한 훅
 export default function useInitUser() {
   const { isLoggedIn } = useUserStore(state => state.userState);
-  const setUser = useUserStore(state => state.setUser);
+  //const setUser = useUserStore(state => state.setUser);
 
   useEffect(() => {
     // 토큰이 있는데 전역 로그인 상태가 아니라면
