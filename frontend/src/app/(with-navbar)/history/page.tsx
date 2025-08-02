8d0d1a79d0069110b8ab5cf786622e320a4b3eae
jjeongsu <jeongsu.dev@gmail.com>
2025-08-02 11:57:15 +0900

    feat: 내 방문 페이지 Error, 로딩 상태 추가

--- frontend/src/app/(with-navbar)/history/page.tsx
+++ frontend/src/app/(with-navbar)/history/page.tsx
@@ -1,13 +1,32 @@
+'use client';
+
 import PopupHistoryList from '@/features/history/ui/PopupHistoryList';
 import { Suspense } from 'react';
 import PopupCardListSuspenseFallback from '@/entities/popup/ui/PopupCardListSuspenseFallback';
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+import FilteredPopupListErrorFallback from '@/features/filtering/ui/FilteredPopupListErrorFallback';
 
 export default function HistoryPage() {
   return (
     <div>
-      <Suspense fallback={<PopupCardListSuspenseFallback />}>
-        <PopupHistoryList />
-      </Suspense>
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary
+            onReset={reset}
+            fallbackRender={({ error, resetErrorBoundary }) => (
+              <FilteredPopupListErrorFallback
+                onRetry={resetErrorBoundary}
+                error={error}
+              />
+            )}
+          >
+            <Suspense fallback={<PopupCardListSuspenseFallback />}>
+              <PopupHistoryList />
+            </Suspense>
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
     </div>
   );
 }
