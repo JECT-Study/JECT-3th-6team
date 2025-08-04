cece0f785747583806b23631699c2c6c8f037ded
jjeongsu <jeongsu.dev@gmail.com>
2025-08-04 16:19:55 +0900

    refactor: AuthInitializer의 유저정보 패칭 방식 변경

--- frontend/src/entities/user/lib/AuthInitializer.tsx
+++ frontend/src/entities/user/lib/AuthInitializer.tsx
@@ -1,18 +1,36 @@
+'use client';
+import { useQuery } from '@tanstack/react-query';
 import getUserApi from '../api/getUserApi';
 import UserInitProvider from '@/entities/user/lib/UserInitProvider';
 
-export default async function AuthInitializer({
+export default function AuthInitializer({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  let user = null;
-  try {
-    user = await getUserApi();
-  } catch (error) {
-    console.error('유저정보 조회에 실패하였습니다.');
-    console.error(error);
+  const {
+    data: user,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['auth', 'user'],
+    queryFn: getUserApi,
+    retry: 1,
+    staleTime: 5 * 60 * 1000, // 5분
+    gcTime: 30 * 60 * 1000, // 30분
+    refetchOnWindowFocus: false,
+  });
+
+  if (isLoading) {
+    return null;
   }
 
-  return <UserInitProvider initialUser={user}>{children}</UserInitProvider>;
+  if (isError) {
+    console.error('유저정보 조회에 실패하였습니다.', error);
+  }
+
+  return (
+    <UserInitProvider initialUser={user ?? null}>{children}</UserInitProvider>
+  );
 }
