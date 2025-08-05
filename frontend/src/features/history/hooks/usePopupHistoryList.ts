3eff03877998bf230b1c752b3a86e9b0dbe0cace
jjeongsu <jeongsu.dev@gmail.com>
2025-08-05 18:57:04 +0900

    fix : gcTime 오타 수정

--- frontend/src/features/history/hooks/usePopupHistoryList.ts
+++ frontend/src/features/history/hooks/usePopupHistoryList.ts
@@ -21,7 +21,7 @@ export default function usePopupHistoryList() {
           : initialRequest;
       return getPopupHistoryListApi({ ...request });
     },
-    gcTime: 1000 * 60 * 300, // 30분
+    gcTime: 1000 * 60 * 30, // 30분
     staleTime: 1000 * 60 * 10, // 10분,
     getNextPageParam: (lastPage: TaggedPopupHistoryListResponse) =>
       lastPage.hasNext ? lastPage.lastWaitingId : null,
