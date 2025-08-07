c02b23d08110ef9d873a82ee009f9d20cd13399f
jjeongsu <jeongsu.dev@gmail.com>
2025-08-07 09:15:14 +0900

    feat: 추가 유틸 함수 구현 (getStoreNameFromNotification, getIdFromNotification)

---
+++ frontend/src/features/notification/lib/getStoreNameFromNotification.ts
@@ -0,0 +1,12 @@
+// Notification 객체에서  팝업스토어 명을 추출하는 함수
+import NotificationType from '@/features/notification/type/Notification';
+
+export const getStoreNameFromNotification = (
+  notification: NotificationType
+) => {
+  const { relatedResource } = notification;
+  const popupObject = relatedResource.find(
+    resource => resource.type === 'POPUP'
+  );
+  return popupObject?.data.storeName;
+};
