01a070ca46875df4c8d9c74e88ab9f72dab6009c
jjeongsu <jeongsu.dev@gmail.com>
2025-08-08 18:56:50 +0900

    feat: 알림 삭제 및 읽음 처리 시 React Query 캐시 무효화 로직 추가, 더미 데이터 제거
    - `useReadNotification`에 `onSuccess` 콜백 추가로 캐시 무효화 로직 구현
    - `useDeleteNotification`에 `onSuccess` 콜백 수정 및 삭제 요청 데이터 구조 변경
    - `useNotificationStore`에서 알림 더미 데이터 제거 및 빈 배열로 초기화

--- frontend/src/features/notification/hook/useReadNotification.ts
+++ frontend/src/features/notification/hook/useReadNotification.ts
@@ -1,10 +1,12 @@
 'use client';
 
 import patchNotificationAsReadApi from '@/features/notification/api/patchNotificationAsReadApi';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
 
 // TODO : Optimistic Update 적용
 export default function useReadNotification() {
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     retry: false,
     mutationFn: (notificationId: number) =>
@@ -12,6 +14,11 @@ export default function useReadNotification() {
     onError: error => {
       console.error('[onError]:', error.message);
       toast.error('알림 내역 읽음처리에 실패했습니다.');
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ['notification', 'list'],
+      });
     },
   });
 
