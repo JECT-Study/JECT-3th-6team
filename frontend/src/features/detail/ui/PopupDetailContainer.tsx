52de3be4385c77d638ae7b2f64730d7743121b6b
hyun <ysk30430@naver.com>
2025-08-19 13:23:07 +0900

    feat: 카메라 기능 대신 기본 카메라 요청 모달 추가

--- frontend/src/features/detail/ui/PopupDetailContainer.tsx
+++ frontend/src/features/detail/ui/PopupDetailContainer.tsx
@@ -17,7 +17,7 @@ export default function PopupDetailContainer({
   });
 
   if (!popupDetailData) {
-    throw new Error('팝업 데이터를 찾을 수 없습니다.');
+    return <div>팝업 데이터를 찾을 수 없습니다.</div>;
   }
 
   return (
