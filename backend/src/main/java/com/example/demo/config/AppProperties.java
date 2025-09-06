df21551a8e20c54257374a73c60fdb7552715e95
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-09-06 17:16:57 +0900

    fix: 운영환경별 cors 허용 설정 (#263)
    * fix: 운영환경별 cors 허용 설정
    
    * fix: 프론트 url로 변경
    
    * fix: cors 허용 도메인에 www 추가

--- backend/src/main/java/com/example/demo/config/AppProperties.java
+++ backend/src/main/java/com/example/demo/config/AppProperties.java
@@ -4,9 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "custom.app")
 public class AppProperties {
     private String frontendUrl;
+    private List<String> corsAllowedOrigins;
 } 
