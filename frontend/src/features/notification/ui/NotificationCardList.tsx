8e289b3b03159f4e7785802f548588ea0d9ff55d
jjeongsu <jeongsu.dev@gmail.com>
2025-09-09 17:58:09 +0900

    chore: 알림삭제에 대한 퍼포먼스 모니터링용 코드 추가

--- frontend/src/features/notification/ui/NotificationCardList.tsx
+++ frontend/src/features/notification/ui/NotificationCardList.tsx
@@ -4,8 +4,11 @@ import useNotificationList from '@/features/notification/hook/useNotificationLis
 import { useIntersectionObserver } from '@/shared/hook/useIntersectionObserver';
 import NotificationCardListView from '@/features/notification/ui/NotificationCardListView';
 import useDeleteNotification from '@/features/notification/hook/useDeleteNotification';
+import NotificationType from '@/features/notification/type/Notification';
+import { useEffect, useRef } from 'react';
 
 export default function NotificationCardList() {
+  const currentDeleteIdRef = useRef<number | null>(null);
   const { data, hasNextPage, isFetchingNextPage, fetchNextPage } =
     useNotificationList();
   const { mutate: deleteNotification } = useDeleteNotification();
@@ -16,11 +19,53 @@ export default function NotificationCardList() {
     }
   });
 
+  const handleDeleteClick = (notification: NotificationType) => {
+    const { notificationId } = notification;
+
+    // 1. 클릭 시작
+    performance.mark(`notification_delete_${notificationId}_start`);
+
+    // 2.상태 없데이트
+    currentDeleteIdRef.current = notificationId; // 현재 삭제할 알림id 기록
+    deleteNotification(notificationId);
+  };
+
+  // refetch 된 이후 ui 갱신 시점
+  useEffect(() => {
+    if (data) {
+      const deleteId = currentDeleteIdRef.current;
+      const stillExists = data.content.some(
+        noti => noti.notificationId === deleteId
+      );
+
+      if (deleteId && !stillExists) {
+        performance.mark(`notification_delete_${deleteId}_end`);
+        performance.measure(
+          `notification_delete_${deleteId}_latency`,
+          `notification_delete_${deleteId}_start`,
+          `notification_delete_${deleteId}_end`
+        );
+
+        const entries = performance.getEntriesByName(
+          `notification_delete_${deleteId}_latency`
+        );
+
+        const duration = entries[0]?.duration;
+
+        if (duration) {
+          console.log(
+            `[Perf] Notification ${deleteId} delete latency: ${duration} ms`
+          );
+        }
+      }
+    }
+  }, [data]);
+
   return (
     <div className="flex flex-col">
       <NotificationCardListView
         data={data.content}
-        handleDelete={deleteNotification}
+        handleDelete={handleDeleteClick}
         lastElementRef={lastElementRef}
       />
     </div>
