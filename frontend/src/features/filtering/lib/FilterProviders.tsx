7c4be329fdd655027c7e6fa064222a1c03a69cab
jjeongsu <jeongsu.dev@gmail.com>
2025-09-23 14:56:20 +0900

    fix: initialFilter 선언 위치 변경

--- frontend/src/features/filtering/lib/FilterProviders.tsx
+++ frontend/src/features/filtering/lib/FilterProviders.tsx
@@ -6,19 +6,18 @@ import { FilterState } from '@/features/filtering/hook/type';
 import useFilter from '@/features/filtering/hook/useFilter';
 import useURLFilterSync from '@/features/filtering/hook/useURLFilterSync';
 
-const initialFilter: FilterState = {
-  date: {
-    start: new Date(),
-    end: addDays(new Date(), 6),
-  },
-  location: '전국',
-  keyword: {
-    popupType: [],
-    category: [],
-  },
-};
-
 const HomeFilterProvider = ({ children }: { children: React.ReactNode }) => {
+  const initialFilter: FilterState = {
+    date: {
+      start: new Date(),
+      end: addDays(new Date(), 6),
+    },
+    location: '전국',
+    keyword: {
+      popupType: [],
+      category: [],
+    },
+  };
   const filter = useFilter(initialFilter);
   // url에 필터 상태를 동기화
   useURLFilterSync({
@@ -33,6 +32,17 @@ const HomeFilterProvider = ({ children }: { children: React.ReactNode }) => {
 };
 
 const MapFilterProvider = ({ children }: { children: React.ReactNode }) => {
+  const initialFilter: FilterState = {
+    date: {
+      start: new Date(),
+      end: addDays(new Date(), 6),
+    },
+    location: '전국',
+    keyword: {
+      popupType: [],
+      category: [],
+    },
+  };
   const filter = useFilter(initialFilter);
 
   return (
